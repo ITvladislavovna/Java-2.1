628591adc7eab4a534ba3a20fb6beb268c7a8e55
ITvladislavovna <irichka-k@rambler.ru>
2022-07-05 09:56:43 +0300

    mili

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int rub = 20;
+        int mile = price/rub;
+                System.out.println("Бонусные мили:"+mile);
+    }
+}
