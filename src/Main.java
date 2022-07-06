8621f87f0ccaa5e8f355467a4678a2ba3c012a77
ITvladislavovna <irichka-k@rambler.ru>
2022-07-06 14:15:14 +0300

    mile

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 5000;
         int rub = 20;
-        int mile = price/rub;
-                System.out.println("Бонусные мили:"+mile);
+        int mile = price / rub;
+        System.out.println("Бонусные мили:" + mile);
     }
 }
